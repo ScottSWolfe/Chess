1cb382026a50dd4f870e49e6c90b478927ba50e8
Scott Wolfe <swolfe8@gatech.edu>
2017-05-23 22:16:17 -0400

    minor display changes

--- Platform/HumanPlayer.cpp
+++ Platform/HumanPlayer.cpp
@@ -13,21 +13,19 @@ Move HumanPlayer::move(Board board) const {
 	cout << "Enter start x coord: ";
 	int start_x;
 	cin >> start_x;
-	cout << endl;
 
 	cout << "Enter start y coord: ";
 	int start_y;
 	cin >> start_y;
-	cout << endl;
 	
 	cout << "Enter end x coord: ";
 	int end_x;
 	cin >> end_x;
-	cout << endl;
 	
 	cout << "Enter end y coord: ";
 	int end_y;
 	cin >> end_y;
+
 	cout << endl;
 
 	SquareCoordinate start = {start_x, start_y};
