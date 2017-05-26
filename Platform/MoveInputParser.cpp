93a2aa2d2f7fd767d4a7a23f7393b42decc477aa
Scott Wolfe <swolfe8@gatech.edu>
2017-05-25 22:27:33 -0400

    improve space delimited token parsing

--- Platform/MoveInputParser.cpp
+++ Platform/MoveInputParser.cpp
@@ -7,39 +7,30 @@ using namespace std;
 
 shared_ptr<const Move> MoveInputParser::parseMoveInput(string input) const {
 
-	// find first two character pairs, space delimited
-	vector<int> space_indexes;
-	for (int i = 0; i < input.size(); i++) {
-		char c = input[i];
-		if (c == ' ') {
-			space_indexes.emplace_back(i);
-		}
-	}
-	space_indexes.emplace_back(input.size());
-
-	vector<string> delimited_tokens;
-	for (int i = 0; i < space_indexes.size(); i++) {
-		int prev_index = -1;
-		if (i != 0) {
-			prev_index = space_indexes[i - 1];
-		}
-		int next_index = space_indexes[i];
-		int length = next_index - prev_index - 1;
-		string token = input.substr(prev_index + 1, length);
-		if (!token.empty()) {
-			delimited_tokens.emplace_back(token);
+	// parse input into tokens
+	vector<string> tokens;
+	input.append(" ");
+	while (!input.empty()) {
+		for (int i = 0; i < input.size(); i++) {
+			char c = input[i];
+			if (c == ' ') {
+				if (i != 0) {
+					tokens.emplace_back(input.substr(0, i));
+				}
+				input = input.erase(0, i + 1);
+			}
 		}
 	}
 
-	// check for at least two coordinate pairs
-	if (delimited_tokens.size() < 2) {
+	// check that there are at least two coordinate pairs
+	if (tokens.size() < 2) {
 		return shared_ptr<const Move>(nullptr);
 	}
 
 	// turn each char pair into SquareCoordinates
 	array<SquareCoordinate, 2> coords;
 	for (int i = 0; i < 2; i++) {
-		string token = delimited_tokens[i];
+		string token = tokens[i];
 		if (token.size() != 2) {
 			return shared_ptr<const Move>(nullptr);
 		}
